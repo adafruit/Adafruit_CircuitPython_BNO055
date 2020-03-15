7e291b2666088f6a1d6373ecb9fc34868dd5f1f2
dherrada <dylan.herrada@gmail.com>
2020-03-15 17:34:45 -0400

    Ran black, updated to pylint 2.x

--- examples/webgl_demo/server.py
+++ examples/webgl_demo/server.py
@@ -37,6 +37,7 @@ from flask import *
 import board
 import busio
 import adafruit_bno055
+
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the BNO sensor connection.
@@ -49,13 +50,13 @@ BNO_UPDATE_FREQUENCY_HZ = 10
 
 # Name of the file to store calibration data when the save/load calibration
 # button is pressed.  Calibration data is stored in JSON format.
-CALIBRATION_FILE = 'calibration.json'
+CALIBRATION_FILE = "calibration.json"
 
 # BNO sensor axes remap values.  These are the parameters to the BNO.set_axis_remap
 # function.  Don't change these without consulting section 3.4 of the datasheet.
 # The default axes mapping below assumes the Adafruit BNO055 breakout is flat on
 # a table with the row of SDA, SCL, GND, VIN, etc pins facing away from you.
-#BNO_AXIS_REMAP = { 'x': BNO055.AXIS_REMAP_X,
+# BNO_AXIS_REMAP = { 'x': BNO055.AXIS_REMAP_X,
 #                   'y': BNO055.AXIS_REMAP_Z,
 #                   'z': BNO055.AXIS_REMAP_Y,
 #                   'x_sign': BNO055.AXIS_REMAP_POSITIVE,
@@ -88,14 +89,15 @@ def read_bno():
         # Capture the lock on the bno_changed condition so the bno_data shared
         # state can be updated.
         with bno_changed:
-            bno_data['euler'] = bno.euler
-            bno_data['temp'] = bno.temperature
-            bno_data['quaternion'] = bno.quaternion
-            bno_data['calibration'] = bno.calibration_status
+            bno_data["euler"] = bno.euler
+            bno_data["temp"] = bno.temperature
+            bno_data["quaternion"] = bno.quaternion
+            bno_data["calibration"] = bno.calibration_status
             # Notify any waiting threads that the BNO state has been updated.
             bno_changed.notifyAll()
         # Sleep until the next reading.
-        time.sleep(1.0/BNO_UPDATE_FREQUENCY_HZ)
+        time.sleep(1.0 / BNO_UPDATE_FREQUENCY_HZ)
+
 
 def bno_sse():
     """Function to handle sending BNO055 sensor data to the client web browser
@@ -113,15 +115,26 @@ def bno_sse():
             bno_changed.wait()
             # A new reading is available!  Grab the reading value and then give
             # up the lock.
-            heading, roll, pitch = bno_data['euler']
-            temp = bno_data['temp']
-            x, y, z, w = bno_data['quaternion']
-            sys, gyro, accel, mag = bno_data['calibration']
+            heading, roll, pitch = bno_data["euler"]
+            temp = bno_data["temp"]
+            x, y, z, w = bno_data["quaternion"]
+            sys, gyro, accel, mag = bno_data["calibration"]
         # Send the data to the connected client in HTML5 server sent event format.
-        data = {'heading': heading, 'roll': roll, 'pitch': pitch, 'temp': temp,
-                'quatX': x, 'quatY': y, 'quatZ': z, 'quatW': w,
-                'calSys': sys, 'calGyro': gyro, 'calAccel': accel, 'calMag': mag }
-        yield 'data: {0}\n\n'.format(json.dumps(data))
+        data = {
+            "heading": heading,
+            "roll": roll,
+            "pitch": pitch,
+            "temp": temp,
+            "quatX": x,
+            "quatY": y,
+            "quatZ": z,
+            "quatW": w,
+            "calSys": sys,
+            "calGyro": gyro,
+            "calAccel": accel,
+            "calMag": mag,
+        }
+        yield "data: {0}\n\n".format(json.dumps(data))
 
 
 @app.before_first_request
@@ -137,37 +150,41 @@ def start_bno_thread():
     bno_thread.daemon = True  # Don't let the BNO reading thread block exiting.
     bno_thread.start()
 
-@app.route('/bno')
+
+@app.route("/bno")
 def bno_path():
     # Return SSE response and call bno_sse function to stream sensor data to
     # the webpage.
-    return Response(bno_sse(), mimetype='text/event-stream')
+    return Response(bno_sse(), mimetype="text/event-stream")
 
-@app.route('/save_calibration', methods=['POST'])
+
+@app.route("/save_calibration", methods=["POST"])
 def save_calibration():
     # Save calibration data to disk.
     #
     # TODO: implement this
     #
-    return 'OK'
+    return "OK"
 
-@app.route('/load_calibration', methods=['POST'])
+
+@app.route("/load_calibration", methods=["POST"])
 def load_calibration():
     # Load calibration from disk.
     #
     # TODO: implement this
     #
-    return 'OK'
+    return "OK"
 
-@app.route('/')
+
+@app.route("/")
 def root():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Create a server listening for external connections on the default
     # port 5000.  Enable debug mode for better error messages and live
     # reloading of the server on changes.  Also make the server threaded
     # so multiple connections can be processed at once (very important
     # for using server sent events).
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host="0.0.0.0", debug=True, threaded=True)
