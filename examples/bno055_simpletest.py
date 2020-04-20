bbcfe400810cd1d59bee209a6c85086ea938a17c
caternuson <caternuson@gmail.com>
2020-04-20 14:51:58 -0700

    update examples

--- examples/bno055_simpletest.py
+++ examples/bno055_simpletest.py
@@ -4,7 +4,7 @@ import busio
 import adafruit_bno055
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_bno055.BNO055(i2c)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 while True:
     print("Temperature: {} degrees C".format(sensor.temperature))
