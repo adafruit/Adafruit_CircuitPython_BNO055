7147de8190f09b19ff7cc1eb0fe3a528d549f543
dherrada <dylan.herrada@adafruit.com>
2020-07-01 15:44:47 -0400

    Formatted

--- adafruit_bno055.py
+++ adafruit_bno055.py
@@ -408,7 +408,7 @@ class BNO055_UART(BNO055):
             pass
         resp = self._uart.read(self._uart.in_waiting)
 
-        if resp[0] != 0xBB: # Recursion
+        if resp[0] != 0xBB:  # Recursion
             self._uart.write(bytes([0xAA, 0x01, register, length]))
             now = time.time()
             while self._uart.in_waiting < length + 2 and time.time() - now < 0.25:
