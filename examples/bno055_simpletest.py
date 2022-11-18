22289f11ab3ba5c7cc8fe0cd88940bd9afb62ea2
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:21 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/bno055_simpletest.py
+++ examples/bno055_simpletest.py
@@ -6,7 +6,8 @@ import board
 import adafruit_bno055
 
 
-i2c = board.I2C()
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 # If you are going to use UART uncomment these lines
