ce465c6f09c703700b4113973a2de07d36a0751c
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2023-05-23 22:16:49 -0400

    Update .pylintrc, fix jQuery for docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,12 @@ sys.path.insert(0, os.path.abspath(".."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.jquery",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode"
+]
 
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
